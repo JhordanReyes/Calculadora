6cb0b3d7f820247b41368e15a627ef5e9e0db406
JhordanReyes <95984454+JhordanReyes@users.noreply.github.com>
2022-06-24 12:50:59 -0400

    Se agregaron nuevas funcionalidades

--- js/operaciones.js
+++ js/operaciones.js
@@ -3,4 +3,8 @@ class Calculos{
     resta = (a , b) => a - b;
     division = (a , b) => a / b;
     multiplicacion = (a , b) => a * b;
+    raiz = (a) => Math.sqrt(a);
+    potenciaAuto = (a) => Math.pow(a,2);
+    potencia = (a,b) => Math.pow(a,b);
+    PI = (a) => a * Math.PI;
 }
