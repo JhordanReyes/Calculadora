fcdce86555d06e687fa41ba31a1cb3280294a563
JhordanReyes <95984454+JhordanReyes@users.noreply.github.com>
2022-06-24 12:50:24 -0400

    Se agregaron funcionalidades mas avanzadas

--- js/Calculadora.js
+++ js/Calculadora.js
@@ -11,11 +11,18 @@ class Calculadora{
             suma: "+",
             resta: "-",
             division: "%",
-            multiplicacion: "x"
+            multiplicacion: "x",
+            raiz: "√",
+            potenciaAuto: "^ 2",
+            potencia: "^",
+            PI: "π"
         };
         this.calculos = new Calculos();
     }
     agregarValor(number){
+        if(this.numberA === "ERROR"){
+            this.numberA = "";
+        }
         if(number === "." && this.numberA.includes(".")){
             this.numberA = this.numberA;
         } else {
@@ -24,10 +31,19 @@ class Calculadora{
         this.imprimirValor();
     };
     imprimirValor(){
+        if(this.operacion === "raiz"){
+            this.primerValor.textContent = `${this.numberA}`;
+            this.segundoValor.textContent = `${this.signos[this.operacion]} ${this.numberB}`
+            return
+        }
         this.primerValor.textContent = `${this.numberA}`;
         this.segundoValor.textContent = `${this.numberB} ${this.signos[this.operacion] || ""}`;
     }
     borrar(){
+        if(this.numberA === "ERROR"){
+            this.borrarTodo();
+            return
+        }
         this.numberA = this.numberA.slice(0,-1);
         this.imprimirValor();
     }
@@ -48,22 +64,42 @@ class Calculadora{
             this.numberA = this.signos[this.signoNumberA];
             this.imprimirValor();
             return
-        } else {
-            this.operacion = tipoOperacion;
-            this.numberB = this.numberA;
-            this.signoNumberB = this.signoNumberA;
-            this.signoNumberA = "";
-            this.numberA = "";
-            this.imprimirValor();
         }
-    }
-    igual(){
-        if(this.numberA === "" || this.numberB === ""){
+        if(tipoOperacion === "raiz" && this.numberA < 0){
+            this.numberA = "ERROR";
+            this.imprimirValor();
             return
         }
+        this.operacion = tipoOperacion;
+        this.numberB = this.numberA;
+        this.signoNumberB = this.signoNumberA;
+        this.signoNumberA = "";
+        this.numberA = "";
+        this.imprimirValor();
+    }
+    igual(){
+        if(this.operacion === "raiz" || this.operacion === "potenciaAuto" || this.operacion === "PI"){
+            this.numberA = this.calculos[this.operacion](this.numberB);
+            if(this.numberA % 1 === 0){
+                this.numberA = String(this.numberA);
+            } else {
+                this.numberA = String( this.numberA.toFixed(5) );
+            }
+            this.numberB = "";
+            this.signoNumberB = "";
+            this.operacion = "";
+            this.imprimirValor();
+            return
+        }
+        if(this.numberA === "" || this.numberB === ""){return}
         this.numberA = parseFloat(this.numberA);
         this.numberB = parseFloat(this.numberB);
-        this.numberA = String(this.calculos[this.operacion](this.numberB, this.numberA));
+        this.numberA = this.calculos[this.operacion](this.numberB, this.numberA);
+        if(this.numberA % 1 === 0){
+            this.numberA = String(this.numberA);
+        } else {
+            this.numberA = String( this.numberA.toFixed(5) );
+        }
         this.numberB = "";
         this.signoNumberB = "";
         this.operacion = "";
